71ed14b5dd057b2595e5a5d31f6ded513671ffc5
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-05 14:38:35 +0200

    add: sintax pagecontroller and import bootstrap

--- app/Http/Controllers/Admin/pagecontroller.php
+++ app/Http/Controllers/Admin/pagecontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class pagecontroller extends Controller
+class Pagecontroller extends Controller
 {
     public function dashboard()
     {
