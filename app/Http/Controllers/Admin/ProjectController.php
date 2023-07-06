d3148d305cc0916c2c9a7942b49c973679a54e4f
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 17:08:19 +0200

    add: function crud

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $project = project::paginate(5);
+
+        return view('admin.posts.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        return view('admin.posts.show', compact('project'));
+    }
 }
