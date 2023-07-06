766f011925e6e661a2f3ccbf688bdb02b3060c05
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 17:30:55 +0200

    fix show

--- resources/views/Admin/posts/show.blade.php
+++ resources/views/Admin/posts/show.blade.php
@@ -1,28 +1,25 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-<h1 class="text-center">Comics</h1>
+<h1 class="text-center">Project</h1>
 <div class="container">
-  <div class="row row-cols-3">
-  
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="{{$comic->thumb}}" class="card-img-top image-fluid" alt=""> 
-        <div class="card-body">
-        <h5 class="card-title">{{$comic->title}}</h5>
-        <p class="card-text">{{$comic->description}}</p>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">{{$comic->series}}</li>
-        <li class="list-group-item">{{$comic->sale_date}}</li>
-        <li class="list-group-item">{{$comic->type}}</li>
-      </ul>
-    <div class="card-body">
-    <a class="btn btn-primary" href="{{ route('comics.show', ['comic' => $comic->id]) }}">View</a>
-  </div>
-</div>
-</div>
-
-</div>
+    <div class="row row-cols-3">
+        @foreach ($projects as $project)
+        <div class="col">
+            <div class="card-body">
+                <h5 class="card-title">{{$project->title}}</h5>
+                <p class="card-text">{{$project->description}}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{$project->date}}</li>
+                <li class="list-group-item">{{$project->name}}</li>
+                <li class="list-group-item">{{$project->surname}}</li>
+            </ul>
+            <div class="card-body">
+                <a class="btn btn-primary" href="{{ route('posts.show', ['project' => $project->id]) }}">VIEW</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 @endsection
