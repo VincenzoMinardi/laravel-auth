e96055b5259311c7467049f61175a803c7055657
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 17:23:32 +0200

    popolate index and show

--- resources/views/Admin/posts/index.blade.php
+++ resources/views/Admin/posts/index.blade.php
@@ -1,5 +1,25 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-    
+<div class="container">
+    <div class="row row-cols-3">
+        @foreach ($projects as $project)
+        <div class="col">
+            <div class="card-body">
+                <h5 class="card-title">{{$project->title}}</h5>
+                <p class="card-text">{{$project->description}}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{$project->date}}</li>
+                <li class="list-group-item">{{$project->name}}</li>
+                <li class="list-group-item">{{$project->surname}}</li>
+            </ul>
+            <div class="card-body">
+                <a class="btn btn-primary" href="{{ route('posts.show', ['project' => $project->id]) }}">VIEW</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+{{ $comics->links() }}
 @endsection
