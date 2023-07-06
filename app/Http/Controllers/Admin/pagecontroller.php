8a722c34bb73a56e2cb2137cda90f0eb787a9c9b
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 15:12:21 +0200

    add Project controller and migration ProjectsTable

--- app/Http/Controllers/Admin/pagecontroller.php
+++ app/Http/Controllers/Admin/pagecontroller.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
 
 class Pagecontroller extends Controller
 {
+
     public function dashboard()
     {
         return view('admin.dashboard');
