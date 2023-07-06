0817930e200490d9b05c823471574f960c615e24
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 16:22:13 +0200

    correction confirm password

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,7 +46,7 @@
         </div>
 
         <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
+            <label for="password" class="form-label"> Confirm Password</label>
             <input
                 type="password"
                 class="form-control"
