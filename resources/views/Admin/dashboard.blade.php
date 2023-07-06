96ae9277bfefe2096dddc9302a05f3217f579c14
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-06 18:08:24 +0200

    fix error

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -3,5 +3,5 @@
 @section('contents')
 
 <h1 class="text-center">Benvenuto </h1>
-{{-- <a class="btn btn-primary" href="{{ route('posts.show', ['project' => $project->id]) }}">VIEW</a> --}}
+
 @endsection
