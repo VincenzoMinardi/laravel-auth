a58ec47652223a16c2d52f910eaeadad9f47d983
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-07 16:09:25 +0200

    add: link logout

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -4,7 +4,7 @@
 
 <p class="text-center h1">Qui troverai tutti miei progetti riguardo al mio percorso fatto con Boolean</p>
 <button class="m-auto button-dashboard">
-    <a class="link" href="/admin/projects">Button</a>
+    <a class="link" href="/admin/projects">Projects</a>
     <div class="top"></div>
     <div class="left"></div>
     <div class="bottom"></div>
--- resources/views/Admin/includes/nav.blade.php
+++ resources/views/Admin/includes/nav.blade.php
@@ -1,3 +1,7 @@
+@php
+    $user = Auth::user();
+@endphp
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('admin.dashboard') }}">BoolFolio</a>
@@ -17,10 +21,21 @@
                     </a>
                 </li>
             </ul>
-            {{-- <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> --}}
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ $user->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
